b85903d4110a3ef3750b173a80c43f2203697ca9
Shivam Bhatia <Shivam.Bhatia@Comvision.io>
2024-04-28 17:32:40 +0530

    done with redux toolkit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FaCartPlus } from "react-icons/fa";
 
 const Navbar = () => {
-    const items = useSelector((state) => state.cart);
-    return (
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-            }}
+  const items = useSelector((state) => state.cart);
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 1rem",
+        background: "#764abc",
+      }}
+    >
+      <div className="header">
+        <img src="./store.jpg" alt="" className="storeLogo" />
+        <h2 className="heading">Welcome to the GRAND STORE</h2>
+      </div>
+      <div className="nav">
+        <Link className="navLink" to="/">
+          Home
+        </Link>
+        <Link
+          className="navLink"
+          to="/cart"
+          style={{ display: "flex", alignItems: "center", gap: "0.3rem" }}
         >
-            <span className="logo">REDUX STORE</span>
-            <div>
-                <Link className="navLink" to="/">
-                    Home
-                </Link>
-                <Link className="navLink" to="/cart">
-                    Cart
-                </Link>
-                <span className="cartCount">Cart items: {items.length}</span>
-            </div>
-        </div>
-    );
+          Cart
+          <FaCartPlus style={{ position: "relative" }} />
+          <span
+            className="cartCount"
+            style={{
+              position: "absolute",
+              top: "2.4rem",
+              color: "#fff",
+              right: "0.3rem",
+            }}
+          >
+            {items.length}
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Products from '../components/Products';
+import React from "react";
+import Products from "../components/Products";
+import "../App.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h2 className="heading">Welcome to the Redux toolkit store</h2>
-            <section>
-                <h3>Products</h3>
-                <Products />
-            </section>
-        </div>
-    );
+  return (
+    <section className="padding">
+      <h3>Products</h3>
+      <Products />
+    </section>
+  );
 };
 
 export default Home;
